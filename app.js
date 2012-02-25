24608573dae073e7ebe04a7a863463704bf16c31
Engin Kurutepe <engin@kurutepe.com>
2012-02-25 17:16:20 +0100

    try postgre connection

--- app.js
+++ app.js
@@ -9,6 +9,13 @@ var express = require('express'),
 
 var pg = require('pg');
 
+pg.connect(process.env.DATABASE_URL, function(err, client) {
+  var query = client.query('SELECT * FROM your_table');
+
+  query.on('row', function(row) {
+    console.log(JSON.stringify(row));
+  });
+});
 
 var app = module.exports = express.createServer();
 
